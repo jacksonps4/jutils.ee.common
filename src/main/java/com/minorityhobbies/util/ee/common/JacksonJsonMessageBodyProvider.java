a55fae0ce558b36691ba1f1c3c4f0f7bd1342f18
Chris Wraith <chris@indigneously.co.uk>
2015-10-17 23:08:59 +0100

    Adding Java8 date/time serialisation

--- src/main/java/com/minorityhobbies/util/ee/common/JacksonJsonMessageBodyProvider.java
+++ src/main/java/com/minorityhobbies/util/ee/common/JacksonJsonMessageBodyProvider.java
@@ -1,28 +1,27 @@
 package com.minorityhobbies.util.ee.common;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 abstract class JacksonJsonMessageBodyProvider {
 	private final ObjectMapper mapper = new ObjectMapper();
 
 	public JacksonJsonMessageBodyProvider() {
 		super();
+		mapper.registerModule(new JavaTimeModule());
 	}
 	
 	protected final void writeValue(Object object, OutputStream stream)
-			throws JsonGenerationException, JsonMappingException, IOException {
+			throws IOException {
 		mapper.writerWithDefaultPrettyPrinter().writeValue(stream, object);
 	}
 	
 	protected final <T> T readValue(InputStream stream, Class<T> type)
-			throws JsonParseException, JsonMappingException, IOException {
+			throws IOException {
 		return mapper.readValue(stream, type);
 	}
 }
